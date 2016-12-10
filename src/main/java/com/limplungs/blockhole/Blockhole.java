9023ef722d663a10d4626ba7c906f2d9410d2473
7319807 <logan.sievers@mines.sdsmt.edu>
2016-12-10 03:08:12 -0700

    initial commit

---
+++ src/main/java/com/limplungs/blockhole/Blockhole.java
@@ -0,0 +1,47 @@
+package com.limplungs.blockhole;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod(modid = Blockhole.MODID, version = Blockhole.VERSION)
+public class Blockhole
+{
+    public static final String MODID = "blockhole";
+    public static final String VERSION = "1.0"; 
+    
+    @EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+    	//BlockList.registerBlocks();
+    	//ItemList.registerItems();
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		if(event.getSide() == Side.CLIENT)
+		{
+			//RenderItem renderer = Minecraft.getMinecraft().getRenderItem();
+
+			//BlockList.renderBlocks(renderer);
+			//ItemList.renderItems(renderer);
+		}
+	}
+	
+	public static CreativeTabs tabBlockhole = new CreativeTabs("tabBlockhole")
+	{
+		@Override
+		@SideOnly(Side.CLIENT)
+		public Item getTabIconItem()
+		{
+			return Items.ENDER_PEARL; //ItemList.item;
+		}
+	};
+}
