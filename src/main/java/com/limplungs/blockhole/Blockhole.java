1aa8a83f377be7ded30f2235542bf7d4464d5d95
7319807 <logan.sievers@mines.sdsmt.edu>
2016-12-10 03:12:09 -0700

    automated commit from MOBILE

--- src/main/java/com/limplungs/blockhole/Blockhole.java
+++ src/main/java/com/limplungs/blockhole/Blockhole.java
@@ -14,7 +14,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class Blockhole
 {
     public static final String MODID = "blockhole";
-    public static final String VERSION = "1.0"; 
+    public static final String VERSION = "1.0";
+    
+    //Ideas
+    //Blockhole Teleporter
+    //Bitreader
     
     @EventHandler
 	public void preInit(FMLPreInitializationEvent event)
