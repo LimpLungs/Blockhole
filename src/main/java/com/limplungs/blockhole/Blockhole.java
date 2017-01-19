a0abb4c8afdc231013e1099319462e8096599a8b
LimpLungs <xlaxlimplungs@gmail.com>
2017-01-19 00:34:20 -0600

    Changed version names to match current development status

--- src/main/java/com/limplungs/blockhole/Blockhole.java
+++ src/main/java/com/limplungs/blockhole/Blockhole.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class Blockhole
 {
     public static final String MODID = "blockhole";
-    public static final String VERSION = "1.0alpha1";
+    public static final String VERSION = "1.0alpha2";
     
     //Ideas
     //Blockhole Teleporter
